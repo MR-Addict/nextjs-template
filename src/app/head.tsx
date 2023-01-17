d0560b1a07fdd7d7344b9dfd3d933ae82452e934
MR-Addict <2750417853@qq.com>
2023-01-17 13:00:01 +0800

    change head title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Nextjs Custom Template</title>
+      <title>Nextjs Template</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='Nextjs Custom Template' />
+      <meta name='description' content='Nextjs Template' />
       <link rel='icon' href='/favicon.png' />
     </>
   );
