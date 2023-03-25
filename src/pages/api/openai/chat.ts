e4721c18ce673d0c12f750c755002482e5d4bf11
MR-Addict <2750417853@qq.com>
2023-03-26 02:35:23 +0800

    try to fix api

--- src/pages/api/openai/chat.ts
+++ src/pages/api/openai/chat.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { ChatRequest } from "@/types";
+import { ChatOptions, Messages } from "@/types";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({ apiKey: process.env.OPENAI_TOKEN });
@@ -10,10 +10,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method !== "POST") return res.setHeader("Allow", ["POST"]).end(`Method ${req.method} is not allowed!`);
   if (req.body.token !== process.env.OPENAI_TOKEN) return res.send("Not authorized");
 
-  const request = ChatRequest.safeParse(req.body);
-  if (!request.success) return res.send("Bad Request");
+  const options = ChatOptions.safeParse(req.body);
+  const messages = Messages.safeParse(JSON.parse(req.body.messages));
+  if (!options.success || !messages.success) return res.send("Bad Request");
 
-  const completion = await openai.createChatCompletion(request.data);
-
+  const completion = await openai.createChatCompletion({ ...options.data, messages: messages.data });
   return res.json(completion);
 }
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,14 @@
 import z from "zod";
 
-const Message = z.object({
-  role: z.enum(["user", "assistant"]),
-  content: z.string(),
-});
+const Messages = z.array(
+  z.object({
+    role: z.enum(["user", "assistant"]),
+    content: z.string(),
+  })
+);
 
-const ChatRequest = z.object({
+const ChatOptions = z.object({
   model: z.enum(["gpt-3.5-turbo"]),
-  messages: z.array(Message),
   temperature: z.number().min(0).max(2).optional(),
   top_p: z.number().min(0).max(1).optional(),
   n: z.number().int().min(1).optional(),
@@ -17,8 +18,8 @@ const ChatRequest = z.object({
   frequency_penalty: z.number().min(-2).max(2).optional(),
 });
 
-type ChatRequestType = z.TypeOf<typeof ChatRequest>;
-type MessageType = z.TypeOf<typeof Message>;
+type ChatOptionsType = z.TypeOf<typeof ChatOptions>;
+type MessagesType = z.TypeOf<typeof Messages>;
 
-export { ChatRequest, Message };
-export type { ChatRequestType, MessageType };
+export { ChatOptions, Messages };
+export type { ChatOptionsType, MessagesType };
