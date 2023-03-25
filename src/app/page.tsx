4bb05c86420b1c794c54f52ba24bcffbab4a110e
MR-Addict <2750417853@qq.com>
2023-03-26 01:46:04 +0800

    add openai chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 import { Tooltip } from "../components";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 export default async function Page() {
-  const session = await unstable_getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   if (!session) return <h1>Please Login!</h1>;
 
--- src/components/Navbar/NavbarButtons.tsx
+++ src/components/Navbar/NavbarButtons.tsx
@@ -1,10 +1,10 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 import LoginLogoutButton from "./LoginLogoutButton";
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
 export default async function NavbarButtons() {
-  const session = await unstable_getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   if (session) return <LoginLogoutButton isNeededLogin={false} />;
   else return <LoginLogoutButton isNeededLogin={true} />;
